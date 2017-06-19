8f2428de8b0f4f518e85bdcce473cef1096d90bb
Sudheer <sudheer@quikchex.in>
2017-06-19 18:18:16 +0530

    meessages added to admin

---
+++ app/admin/message.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register Message do
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+
+  actions :index, :destroy
+  index do
+    column :body
+    column :listing do |message|
+      link_to(message.chat_room.listing.name, detail_listings_path(message.chat_room.listing.slug))
+    end
+    column :receiver, sortable: true
+    column :sender, sortable: true
+    column :created_at
+    column :read
+  end
+end
