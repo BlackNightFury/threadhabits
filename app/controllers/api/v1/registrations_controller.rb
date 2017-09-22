c79bbd674d48325a2013e15fa9d7e5e8ce59f317
Arvind Kushwah <arvindkushwah9@gmail.com>
2017-09-22 18:45:43 +0530

    chnages in api

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::RegistrationsController < ActionController::Base
     user.password_confirmation = person_params[:password]
     user.skip_confirmation! unless user.nil?
     if user.save
-      render json: user.as_json, status: :created
+      render json: user.as_json.merge(avatar_url: user.avatar.try(:url), cover_url: user.cover_image.try(:url), location: user.address, notification_preferences: user.preferences.notifications), status: :created
     else
       warden.custom_failure!
       render json: user.errors, status: :unprocessable_entity
--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::SessionsController < ActionController::Base
 		person = Person.find_by_email_or_username(params[:email])
 		if person&.valid_password?(params[:password])
 			person.save
-			render json: person.as_json, status: :created
+			render json: person.as_json.merge(avatar_url: person.avatar.try(:url), cover_url: person.cover_image.try(:url), location: person.address, notification_preferences: person.preferences.notifications), status: :created
 		else
 			render json: {message: "Email/Username or password are incorrect"}, status: :unauthorized
 		end
